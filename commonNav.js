c92482a40fcd954f76fb58456fe80202606c20a1
chkim-epm <chkim.epm@gmail.com>
2026-01-19 13:11:26 +0900

    Update commonNav.js

--- commonNav.js
+++ commonNav.js
@@ -3,9 +3,10 @@
   if (!holder) return;
 
   try {
-    const res = await fetch("/partials/nav.html");
+    const res = await fetch("/nav.html");
     holder.innerHTML = await res.text();
   } catch (e) {
     console.error("네비 로딩 실패", e);
   }
 })();
+
